5646e767aeb5d1e39bbf236b67acc3ad97f09c81
dzp62442 <dzp62442@163.com>
2026-05-07 16:23:22 +0800

    max_epochs设为20，与论文一致

--- configs/gausstr_featup.py
+++ configs/gausstr_featup.py
@@ -181,7 +181,7 @@ optim_wrapper = dict(
     optimizer=dict(type='AdamW', lr=2e-4, weight_decay=5e-3),
     clip_grad=dict(max_norm=35, norm_type=2))
 
-train_cfg = dict(type='EpochBasedTrainLoop', max_epochs=24, val_interval=1)
+train_cfg = dict(type='EpochBasedTrainLoop', max_epochs=20, val_interval=1)
 val_cfg = dict(type='ValLoop')
 test_cfg = dict(type='TestLoop')
 
